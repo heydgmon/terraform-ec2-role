c700de08b91f8c128a6e307d0b68f767da0df653
heydgmon <40292371+heydgmon@users.noreply.github.com>
2026-06-22 10:55:51 +0900

    initial terraform project

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 terraform {
+  required_version = ">= 1.5"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -9,17 +11,4 @@ terraform {
 
 provider "aws" {
   region = "ap-northeast-2"
-
-  assume_role {
-    role_arn = "arn:aws:iam::604227045332:role/TerraformExecutionRole"
-  }
-}
-
-resource "aws_instance" "test" {
-  ami           = "ami-xxxxxxxxxxxxxxxxx"
-  instance_type = "t3.micro"
-
-  tags = {
-    Name = "terraform-role-test"
-  }
 }
