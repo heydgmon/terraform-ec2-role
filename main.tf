4372c68811c1b2be00eca05f6363fed4c74f787f
heydgmon <40292371+heydgmon@users.noreply.github.com>
2026-06-22 11:18:28 +0900

    terraform 배포 테스트

--- main.tf
+++ main.tf
@@ -12,3 +12,46 @@ terraform {
 provider "aws" {
   region = "ap-northeast-2"
 }
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_ami" "amazon_linux_2023" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*-x86_64"]
+  }
+}
+
+resource "aws_instance" "test" {
+  ami           = data.aws_ami.amazon_linux_2023.id
+  instance_type = "t3.micro"
+  subnet_id     = data.aws_subnets.default.ids[0]
+
+  tags = {
+    Name = "github-actions-terraform-ec2"
+  }
+}
+
+output "vpc_id" {
+  value = data.aws_vpc.default.id
+}
+
+output "subnet_id" {
+  value = data.aws_subnets.default.ids[0]
+}
+
+output "ec2_id" {
+  value = aws_instance.test.id
+}
